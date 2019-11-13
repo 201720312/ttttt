a919540bbdf6d43db8053696261291d9ec42820c
201720312 <2534589653@qq.com>
2019-11-13 23:41:10 +0800

    实验一

---
+++ src/ttttt/njjj.java
@@ -0,0 +1,32 @@
+package ttttt;
+
+public class njjj {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m=0,n=0,ov=0,q=0;
+		char o='+';
+		for(int i=0;i<150;i++){
+			ov=(int)(Math.random()*2);
+			m=(int)(Math.random()*100);
+			n=(int)(Math.random()*100);
+			if(ov==1){
+				o='+';{
+					if(m+n>100)
+						continue;
+				}
+			}else {
+				o='-';{
+					if(m-n<0)
+						continue;
+				}
+			}
+			q++;
+			if(q>50)
+				continue;
+				
+			System.out.println(""+q+":\t"+m+o+n+"=");
+		}
+	}
+
+}
